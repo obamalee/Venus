144d85dd47b7f6c13649fdc9397693ac3068966d
Unknown <obamaisme850510@gmail.com>
2017-10-21 16:42:54 +0800

    1021
    1021

--- app/src/main/java/com/example/obama/venus/gift_box_used.java
+++ app/src/main/java/com/example/obama/venus/gift_box_used.java
@@ -1,7 +1,6 @@
 package com.example.obama.venus;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -23,7 +22,7 @@ import org.json.JSONObject;
 
 import java.io.InputStream;
 
-public class gift_box extends AppCompatActivity {
+public class gift_box_used extends AppCompatActivity {
 
     //session
     public static final String MyPREFERENCES = "MyPrefs" ;
@@ -33,7 +32,7 @@ public class gift_box extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gift_box);
+        setContentView(R.layout.gift_box_used);
 
         //抓取 mb_id
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
@@ -56,69 +55,49 @@ public class gift_box extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                gift_box.this.finish();
-            }
-        });
-
-        Button button27 = (Button)findViewById(R.id.button27);
-        button27.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(gift_box.this, gift_box_used.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                gift_box_used.this.finish();
             }
         });
 
         LinearLayout gift_list = (LinearLayout)findViewById(R.id.gift_list);
         try {
-            final String result = DBConnector.executeQuery("SELECT * FROM gift_box INNER JOIN gift ON gift.gift_id = gift_box.gift_id INNER JOIN shop ON gift.sh_id = shop.sh_id WHERE mb_id = '"+my_id+"' AND used = 'F' ");
+            final String result = DBConnector.executeQuery("SELECT * FROM gift_box INNER JOIN gift ON gift.gift_id = gift_box.gift_id INNER JOIN shop ON gift.sh_id = shop.sh_id WHERE mb_id = '"+my_id+"' AND used = 'T' ");
 
             JSONArray jsonArray = new JSONArray(result);
             for(int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonData = jsonArray.getJSONObject(i);
 
                 final String gift_id = jsonData.getString("gift_id");
-                LinearLayout gift_aaa = new LinearLayout(gift_box.this);
+                LinearLayout gift_aaa = new LinearLayout(gift_box_used.this);
                 gift_aaa.setOrientation(LinearLayout.HORIZONTAL);
                 LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
                 layoutParams.setMargins(0,30,0,0);
-                gift_aaa.setOnClickListener(new Button.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent();
-                        intent.setClass(gift_box.this, qrcode.class);
-                        startActivity(intent);
-                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                        gift_box.this.finish();
-                    }
-                });
 
-                LinearLayout img = new LinearLayout(gift_box.this);
+
+                LinearLayout img = new LinearLayout(gift_box_used.this);
                 img.setOrientation(LinearLayout.VERTICAL);
 
-                ImageView gift_img = new ImageView(gift_box.this);
+                ImageView gift_img = new ImageView(gift_box_used.this);
                 new DownloadImageTask(gift_img)
                         .execute(jsonData.getString("gift_pic"));
                 gift_img.setScaleType(ImageView.ScaleType.FIT_XY);
                 LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(200, 200);
                 layoutParams1.setMargins(15,0,0,0);
 
-                LinearLayout text = new LinearLayout(gift_box.this);
+                LinearLayout text = new LinearLayout(gift_box_used.this);
                 text.setOrientation(LinearLayout.VERTICAL);
                 LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
                 layoutParams2.setMargins(30,0,0,0);
                 text.setBackground(this.getResources().getDrawable(R.drawable.bottom_line_black));
 
-                TextView gift_name = new TextView(gift_box.this);
+                TextView gift_name = new TextView(gift_box_used.this);
                 gift_name.setText(jsonData.getString("gift_name"));
                 gift_name.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
                 gift_name.setPadding(0,0,0,10);
                 gift_name.setTextSize(18);
                 gift_name.setTextColor(Color.BLACK);
 
-                TextView gift_store = new TextView(gift_box.this);
+                TextView gift_store = new TextView(gift_box_used.this);
                 gift_store.setText("請至"+jsonData.getString("sh_name")+"兌換");
                 gift_store.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
                 gift_store.setPadding(0,0,0,10);
--- app/src/main/java/com/example/obama/venus/login.java
+++ app/src/main/java/com/example/obama/venus/login.java
@@ -12,7 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+ import android.widget.ImageButton;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
